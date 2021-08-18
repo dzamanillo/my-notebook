4052301ec937c9ce03e96fa5f1747e1ac7ea26d0
Daniel Zamanillo <dzamanillok@gmail.com>
2021-08-17 21:09:25 -0500

    basic html routes working

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require("express");
-
+const htmlRoutes = require("./routes/htmlRoutes/htmlRoutes.js");
 const app = express();
 
 const PORT = process.env.PORT || 3001;
+
+app.use("/", htmlRoutes);
 
 app.get("/", (req, res) => {
 	res.send("Hello World");
